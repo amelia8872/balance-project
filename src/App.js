5928c6b4f6ff1e30d3e6ea6a39ee9514362de539
amelia8872 <jkramelia8872@gmail.com>
2024-05-15 13:09:44 -0400

    balance project component established

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ListEntries from './ListEntries';
 
 function App() {
+
+  const [entries, setEntries] = useState({
+    income: [],
+    expense: []
+  })
+
+  const [type, setType] = useState('income');
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+  const balance = 0;
+
+
+  const handleTypeChange = (event) => {
+    // console.log(event.target.value);
+    setType(event.target.value);
+  }
+
+  const handleDescriptionChange = (event) => {
+    // console.log(event.target.value);
+    setDescription(event.target.value);
+  }
+
+  const handleAmountChange = (event) => {
+    // console.log(event.target.value);
+    setAmount(event.target.value);
+  }
+
+  const handleAddEntry = () => {
+    console.log('Add Entry');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label>Balance: ${balance}</label>
+      </div>
+
+      <div>
+        <select value={type} onChange={handleTypeChange}>
+          <option value="income">Income</option>
+          <option value="expense">Expense</option>
+        </select>
+
+        <input type="text" value={description} onChange={handleDescriptionChange} placeholder="Description" />
+        <input type="number" value={amount} onChange={handleAmountChange} placeholder="Amount" />
+        <button onClick={handleAddEntry}>Add</button>
+      </div>
+
+      <div>
+        <ListEntries entries={entries.income} />
+        <ListEntries entries={entries.expense} />
+      </div>
     </div>
   );
 }
---
+++ src/ListEntries.js
@@ -0,0 +1,9 @@
+function ListEntries() {
+  return (
+    <div>
+      <h2>Entries</h2>
+    </div>
+  )
+}
+
+export default ListEntries;
